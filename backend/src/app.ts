8f6e59b6b3c71018a17d303e403ab0cfb9fd0378
Flavio Duarte <flavio.duarte@trustly.com>
2023-02-09 23:30:59 -0300

    backend - refactor to manual cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,10 @@
 import { setupApiRoutes } from './api/routes'
-import cors from 'cors'
 import express from 'express'
+import { cors } from './cors'
 
 const app = express()
 
 app.use(cors)
-
 setupApiRoutes(app)
 
 app.get('/', (req, res) => {
